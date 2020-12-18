ff5d2d73b63fd3e2cb163a884182d65fc0e973c0
Rudin Andrey <rudinandrey@yandex.ru>
2020-12-19 00:31:17 +0300

    Base structure of components

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  );
+}
+
+const AppHeader = () => {
+  return <h1>My todo list</h1>;
+}
+
+const SearchPanel = () => {
+  return <input placeholder="search" />;
+}
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
