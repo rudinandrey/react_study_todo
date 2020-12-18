94fcfed00dd9e6288564008e7d1a877b25a2735c
Rudin Andrey <rudinandrey@yandex.ru>
2020-12-19 02:38:50 +0300

    refactoring project structure

---
+++ src/components/app/app.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+
+import './app.css';
+
+const App = () => {
+    const todoData = [
+        { label: 'Drink Coffee', important: false, id: 1 },
+        { label: 'Build React App', important: true, id: 2 },
+        { label: 'Have a lunch', important: false, id: 3 }
+    ];
+    return (
+        <div className="container">
+            <div className="row  form-group">
+                <div className="col">
+                    <AppHeader />
+                </div>
+            </div>
+            <div className="row form-group">
+                <div className="col">
+                    <SearchPanel />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <TodoList todos={todoData} />
+                </div>
+            </div>
+        </div >
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
+import App from './components/app';
 
 
-const App = () => {
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Build React App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
-  return (
-    <div className="container">
-      <div className="row  form-group">
-        <div className="col">
-          <AppHeader />
-        </div>
-      </div>
-      <div className="row form-group">
-        <div className="col">
-          <SearchPanel />
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <TodoList todos={todoData} />
-        </div>
-      </div>
-    </div >
-  );
-}
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
