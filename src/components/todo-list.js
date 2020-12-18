b8cb9583bef5fa0ec84ef8267244831928a97622
Rudin Andrey <rudinandrey@yandex.ru>
2020-12-19 01:09:11 +0300

    Take out item into separate component

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import TodoListItem from './todo-list-item';
 
 const TodoList = () => {
     const items = ['Learn React', 'Build Awesome App'];
     return (
         <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
+            <li><TodoListItem /></li>
+            <li><TodoListItem /></li>
         </ul>
     );
 }
