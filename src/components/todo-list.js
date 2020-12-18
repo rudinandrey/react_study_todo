c6b2190543cef3f2dd6bfd820874fdcc2ed05ed4
Rudin Andrey <rudinandrey@yandex.ru>
2020-12-19 01:25:30 +0300

    create data object for todo app

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TodoListItem from './todo-list-item';
 
 const TodoList = () => {
-    const items = ['Learn React', 'Build Awesome App'];
+
     return (
         <ul>
             <li><TodoListItem label="Drink Coffee" /></li>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import TodoList from './components/todo-list';
 
 
 const App = () => {
-
+  const todoData = [
+    { label: 'Drink Coffee', important: false },
+    { label: 'Build React App', important: true },
+    { label: 'Have a lunch', important: false }
+  ];
   return (
     <div>
       <AppHeader />
