cc06bb466cd58365692d53ee773905aefb53fc86
Rudin Andrey <rudinandrey@yandex.ru>
2020-12-19 11:56:10 +0300

    add state for TodoListItem

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,27 +4,51 @@ import './todo-list-item.css';
 
 
 class TodoListItem extends Component {
+
+    state = {
+        done: false
+    };
+
+    onLabelClick = () => {
+        console.log(`Click ${this.props.label}`);
+        this.setState({
+            done: !this.state.done
+        });
+    };
+
+
     render() {
 
         const { label, important = false } = this.props;
+        const { done } = this.state;
         const style = {
-            color: important ? 'tomato' : 'black'
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
         };
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
+
         return (
-            <span className="todo-list-item" style={style}>
-                <span className="todo-list-item-label">
+            <span className={classNames} >
+                <span
+                    className="todo-list-item-label"
+                    style={style}
+                    onClick={this.onLabelClick}>
                     {label}
                 </span>
 
                 <div className="todo-list-item-buttons">
-                    <button className="btn btn-danger">
+                    <button className="btn btn-danger action-button">
                         <i className="fa fa-trash" aria-hidden="true"></i>
-                    </button>&nbsp;
+                    </button>
                     <button className="btn btn-success">
                         <i className="fa fa-bolt" aria-hidden="true"></i>
                     </button>
                 </div>
-            </span>
+            </span >
         );
     }
 }
