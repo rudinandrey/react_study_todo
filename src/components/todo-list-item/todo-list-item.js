488fee01f08c4b09b75a882532f7586253c5563f
Rudin Andrey <rudinandrey@yandex.ru>
2020-12-19 12:04:22 +0300

    add make important TodoListItem

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -6,7 +6,8 @@ import './todo-list-item.css';
 class TodoListItem extends Component {
 
     state = {
-        done: false
+        done: false,
+        important: false
     };
 
     onLabelClick = () => {
@@ -16,26 +17,32 @@ class TodoListItem extends Component {
         });
     };
 
+    onMarkImportant = () => {
+        this.setState({
+            important: !this.state.important
+        });
+    }
+
 
     render() {
 
-        const { label, important = false } = this.props;
-        const { done } = this.state;
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
+        const { label } = this.props;
+        const { done, important } = this.state;
+
 
         let classNames = 'todo-list-item';
         if (done) {
             classNames += ' done';
         }
 
+        if (important) {
+            classNames += ' important';
+        }
+
         return (
             <span className={classNames} >
                 <span
                     className="todo-list-item-label"
-                    style={style}
                     onClick={this.onLabelClick}>
                     {label}
                 </span>
@@ -44,7 +51,7 @@ class TodoListItem extends Component {
                     <button className="btn btn-danger action-button">
                         <i className="fa fa-trash" aria-hidden="true"></i>
                     </button>
-                    <button className="btn btn-success">
+                    <button className="btn btn-success" onClick={this.onMarkImportant}>
                         <i className="fa fa-bolt" aria-hidden="true"></i>
                     </button>
                 </div>
