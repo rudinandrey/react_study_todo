e212603af4ad7796e1f321543545a918d72a5d08
Rudin Andrey <rudinandrey@yandex.ru>
2020-12-21 01:13:09 +0300

    remove state from todo-list-item and make it function again

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,66 +3,37 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 
-class TodoListItem extends Component {
+const TodoListItem = ({ label, onDeleted, onToggleImportant, onToggleDone, done, important }) => {
 
-    state = {
-        done: false,
-        important: this.props.important
-    };
-
-    onLabelClick = () => {
-        console.log(`Click ${this.props.label}`);
-        this.setState(({ done }) => {
-            return {
-                done: !done
-            }
-        });
-    };
-
-    onMarkImportant = () => {
-        this.setState(({ important }) => {
-            return {
-                important: !important
-            }
-        });
+    let classNames = 'todo-list-item';
+    if (done) {
+        classNames += ' done';
     }
 
-
-    render() {
-
-        const { label, onDeleted, onToggleImportant, onToggleDone } = this.props;
-        const { done, important } = this.state;
-
-
-        let classNames = 'todo-list-item';
-        if (done) {
-            classNames += ' done';
-        }
-
-        if (important) {
-            classNames += ' important';
-        }
-
-        return (
-            <span className={classNames} >
-                <span
-                    className="todo-list-item-label"
-                    onClick={onToggleDone}>
-                    {label}
-                </span>
-
-                <div className="todo-list-item-buttons">
-                    <button className="btn btn-danger action-button" onClick={onDeleted}>
-                        <i className="fa fa-trash" aria-hidden="true"></i>
-                    </button>
-                    <button className="btn btn-success" onClick={onToggleImportant}>
-                        <i className="fa fa-bolt" aria-hidden="true"></i>
-                    </button>
-                </div>
-            </span >
-        );
+    if (important) {
+        classNames += ' important';
     }
+
+    return (
+        <span className={classNames} >
+            <span
+                className="todo-list-item-label"
+                onClick={onToggleDone}>
+                {label}
+            </span>
+
+            <div className="todo-list-item-buttons">
+                <button className="btn btn-danger action-button" onClick={onDeleted}>
+                    <i className="fa fa-trash" aria-hidden="true"></i>
+                </button>
+                <button className="btn btn-success" onClick={onToggleImportant}>
+                    <i className="fa fa-bolt" aria-hidden="true"></i>
+                </button>
+            </div>
+        </span >
+    );
 }
+
 
 
 export default TodoListItem;
